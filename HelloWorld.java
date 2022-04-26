bb32f083220a542c8c41698cc1ef9e124f6749ee
Vincent La <vtla101@gmail.com>
2022-04-26 15:27:25 +1000

    webhook attempt

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("****Hello World and Welcome to Jenkins Training****");
-		System.out.println("These new changes will reflect on the job within Jenkins");
+		System.out.println("These new changes are in relation to github-webhooks");
 	}
 
 }
